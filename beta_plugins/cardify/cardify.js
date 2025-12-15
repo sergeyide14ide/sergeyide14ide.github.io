16e48b9fb8b2e9b11fe5923b5fe397188f26cb95
sergeyide14ide <38718739+sergeyide14ide@users.noreply.github.com>
2025-12-16 01:39:14 +0400

    Update cardify.js

--- beta_plugins/cardify/cardify.js
+++ beta_plugins/cardify/cardify.js
@@ -312,6 +312,14 @@
 .full-start__background {
     height: calc(100% + 6em);
     left: 0 !important;
+    opacity: 0;
+    transition: opacity 0.6s ease-out;
+    animation: none !important;
+    transform: none !important;
+}
+
+.full-start__background.loaded:not(.dim) {
+    opacity: 1 !important;
 }
 
 body:not(.menu--open) .full-start__background {
